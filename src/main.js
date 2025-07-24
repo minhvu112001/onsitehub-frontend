0783bb873c4e48759697517fec6a8bc98fe4a5ae
Your Name <you@example.com>
2025-07-24 14:50:29 +0700

    ok

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css' 
-impaxios.defaults.baseURL = 'http://localhost:8000' 
+import axios from 'axios'
 createApp(App).use(router).mount('#app')
 import { createPinia } from 'pinia'
 const pinia = createPinia()
