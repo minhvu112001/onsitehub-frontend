93ff2fc4ffa41d20ca8e29bef55b3b6208b6c633
Your Name <you@example.com>
2025-07-25 08:57:05 +0700

    ok

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import pinia from './stores'
 import './assets/main.css'
  createApp(App).use(router).mount('#app')
 import { createPinia } from 'pinia'
