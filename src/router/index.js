69c97c68620072512edf8484f25ae8450dfc79f1
Your Name <you@example.com>
2025-07-25 08:02:30 +0700

    ok

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Login from '../pages/Login.vue'
 import Dashboard from '../pages/Dashboard.vue'
+import DashboardCard from '@/components/admin/DashboardCard.vue'
 import Register from '../pages/Register.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/login', name: 'Login', component: Login },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/admin/dashboard', name: 'DashboardCard', component: DashboardCard },
   {path: '/register', name: 'Register', component: Register },
 ]
 
